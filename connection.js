b2bc107ccd4618d7838c774f42660390562e235b
syaiful46 <syaifulramadhan31121999@gmail.com>
2024-02-21 21:22:40 +0700

    ubah host dll

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "ksi-test",
+  host: "blmkdkerrxv24ivuwpzo-mysql.services.clever-cloud.com",
+  user: "utlebmltv5som77q",
+  password: "NX31hDYYMbMSPL7trfAl",
+  database: "blmkdkerrxv24ivuwpzo",
 });
 
 module.exports = db
