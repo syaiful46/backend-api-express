8ae404da6c435babedd02fe5add8164d640316d1
syaiful46 <syaifulramadhan31121999@gmail.com>
2024-02-25 20:49:44 +0700

    add Update and Delete data

--- index.js
+++ index.js
@@ -59,6 +59,43 @@ app.post("/datasekolah", (req, res) => {
   });
 });
 
+app.put("/datasekolah", (req, res) => {
+  const { sekolah_tipe, sekolah_alamat, sekolah_kpos, sekolah_provinsi, sekolah_kota, sekolah_telp, sekolah_email, sekolah_facebook, sekolah_siswa, sekolah_nama } = req.body;
+
+  const sql = `UPDATE datasekolah SET sekolah_tipe = '${sekolah_tipe}', sekolah_alamat = '${sekolah_alamat}', sekolah_kpos = ${sekolah_kpos}, sekolah_provinsi = '${sekolah_provinsi}', sekolah_kota = '${sekolah_kota}', sekolah_telp = ${sekolah_telp}, sekolah_email = '${sekolah_email}', sekolah_facebook = '${sekolah_facebook}', sekolah_siswa = ${sekolah_siswa} WHERE sekolah_nama = '${sekolah_nama}'`;
+  
+  db.query(sql, (err, fields) => {
+    if (err) response(500, "invalid", err, res);
+    if (fields?.affectedRows) {
+      const data = {
+        isSuccess: fields.affectedRows,
+        message: fields.message,
+        data: req.body,
+      };
+      response(200, data, "sekolah updated successfully", res);
+    } else {
+      response(404, "not found", "sekolah not found", res);
+    }
+  });
+});
+
+app.delete("/datasekolah", (req, res) => {
+  const { sekolah_nama } = req.body
+  const sql = `DELETE FROM datasekolah WHERE sekolah_nama = '${sekolah_nama}'`
+  db.query(sql, (err, fields) => {
+    if (err) response(500, 'invalid', 'error', res);
+    if (fields?.affectedRows) {
+      const data = {
+        isDeleted: fields.affectedRows,
+      }
+      response(200, data, 'Deleted school successfully', res)
+    } else {
+      response(404, 'not found', 'sekolah not found', res)
+    }
+  }) 
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
